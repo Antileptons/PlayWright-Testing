1ce2ac0923e3a46460b6169b66e0a38bfef9cdad
Your Name <garzaestradaleonardo@gmail.com>
2024-10-10 18:31:24 -0600

    Testing Push 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,9 +27,9 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-
+    headless: false, // Set to false for non-headless mode
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    /* trace: 'on-first-retry',*/
   },
 
   /* Configure projects for major browsers */
